3f6a3e8f9e1ea00dec8d5a6138febbb45906ab17
khantminht <khantminht.bizs@gmail.com>
2022-09-03 06:58:51 +0630

    3Sept2022

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+// Start jQuery Area \\
 $(document).ready(function(){
-    console.log("hi");
+    
+    // Start Header
+        // Start nav
+        $(".navbuttons").click(function(){
+            $(this).toggleClass("crossx");
+        });
+        // End nav
+    // End Header
 
 
 
@@ -30,4 +38,41 @@ $(document).ready(function(){
 
 
     
+});
+
+// Stat Javascript Ara \\
+
+// Start Student Counter Section
+
+// End Student Counter Section
+
+var getcountervalues = document.querySelectorAll(".countervalues");
+console.log(getcountervalues);
+
+getcountervalues.forEach(function(getcountervalue){
+
+    getcountervalue.textContent = 0; 
+//    console.log(getcountervalue);
+
+    const updatecounter = function(){
+        // console.log('hi');
+
+        const getcountertarget = +getcountervalue.getAttribute('data-target');
+        // console.log(getcountertarget);
+
+        const getcountertext = +getcountervalue.innerText;
+        // console.log(getcountertext);
+
+        const incnumber = getcountertarget / 100;
+        // console.log(incnumber);
+
+        if(getcountertext < getcountertarget){
+            getcountervalue.textContent = getcountertext +incnumber;
+            
+            setTimeout(updatecounter,50);
+        }
+    }
+
+    updatecounter();
+
 });
