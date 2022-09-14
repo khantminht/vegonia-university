382c6514864af26cad94ad73d51883e27c849c88
khantminht <khantminht.bizs@gmail.com>
2022-09-14 22:09:13 +0630

    9FM

--- js/app.js
+++ js/app.js
@@ -40,12 +40,9 @@ $(document).ready(function(){
     
 });
 
-// Stat Javascript Ara \\
+//--------------- Stat Javascript Ara ---------------\\
 
-// Start Student Counter Section
-
-// End Student Counter Section
-
+// Start Student Counter Section\\
 var getcountervalues = document.querySelectorAll(".countervalues");
 console.log(getcountervalues);
 
@@ -76,3 +73,39 @@ getcountervalues.forEach(function(getcountervalue){
     updatecounter();
 
 });
+//--------------- End Student Counter Section---------------\\
+
+
+//--------------- Start Rating SEction ---------------\\
+
+// start google chart api\\
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Myanmar',     8],
+    ['Thailand',      2],
+    ['Singapore',  4],
+    ['Indonesia', 2],
+    ['Srilanka',    8]
+  ]);
+
+  var options = {
+    title: 'International Students',
+    // is3D:true    ==> for 3D pie chart
+    // pieHole:0.3
+    
+    width:550,
+    height:400
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+}
+//end google chart api\\
+
+//--------------- End Rating Section ---------------\\
